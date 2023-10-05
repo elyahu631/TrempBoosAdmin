9571c48beab8bd823468977b051d722c09656037
ElyahuAnavi <84002556+elyahu631@users.noreply.github.com>
2023-10-05 13:51:01 +0300

    update home

--- src/Pages/PHome.jsx
+++ src/Pages/PHome.jsx
@@ -1,37 +1,65 @@
-import { Box, Button } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import { LoginContext } from "../Contexts/LoginContext";
+import { useContext } from 'react';
 
 const PHome = () => {
   const navigate = useNavigate();
+  const { user } = useContext(LoginContext);
 
   const handleNavigation = (path) => {
     navigate(path);
   }
 
   const buttonStyle = {
-    fontWeight: 'bold', 
+    fontWeight: 'bold',
     border: 'solid 3px',
-    fontFamily:'Arial'
+    fontFamily: 'Arial',
+    marginTop: '1rem'  // give space between buttons
   };
 
   return (
-    <Box 
-      display="flex" 
-      flexDirection={{ xs: 'column', sm: 'row' }} 
-      p={2} 
-      height="80vh" 
-      alignItems="center" 
-      justifyContent="space-around" 
+    <Box
+      display="flex"
+      flexDirection="column"
+      p={2}
+      height="80vh"
     >
-      <Box display="flex" flexDirection="column" gap={2} >
-        <Button  style={buttonStyle}  variant="outlined" onClick={() => handleNavigation('/users')}>Users</Button>
-        <Button  style={buttonStyle} variant="outlined" onClick={() => handleNavigation('/groups')}>Groups</Button>
-        <Button  style={buttonStyle} variant="outlined" onClick={() => handleNavigation('/tremps')}>Tremps</Button>
-      </Box>
-      <Box   display="flex" flexDirection="column" gap={2} >
-        <Button  style={buttonStyle} variant="outlined" onClick={() => handleNavigation('/manage-system-admin')}>Managing System Admin</Button>
-        <Button  style={buttonStyle} variant="outlined" onClick={() => handleNavigation('/reports-and-statistics')}>Statistics Dashboard</Button>
-        <Button  style={buttonStyle} variant="outlined" onClick={() => handleNavigation('/group-request')}>Group Request</Button>
+      {user && (
+        <Box
+        display="flex"
+        flexDirection={{ xs: 'column', sm: 'row' }}
+        p={2}
+        alignItems="center"
+        gap={2}
+      >        
+          <Typography variant="h5" component="h2" >
+            Welcome, {user.first_name} {user.last_name}!
+          </Typography>
+          <Typography>
+             ({user.email})
+          </Typography>
+        </Box>
+      )}
+
+      <Box
+        display="flex"
+        flexDirection={{ xs: 'column', sm: 'row' }}
+        p={2}
+        height="80vh"
+        alignItems="center"
+        justifyContent="space-around"
+      >
+        <Box display="flex" flexDirection="column" gap={2} >
+          <Button style={buttonStyle} variant="outlined" onClick={() => handleNavigation('/users')}>Users</Button>
+          <Button style={buttonStyle} variant="outlined" onClick={() => handleNavigation('/groups')}>Groups</Button>
+          <Button style={buttonStyle} variant="outlined" onClick={() => handleNavigation('/tremps')}>Tremps</Button>
+        </Box>
+        <Box display="flex" flexDirection="column" gap={2} >
+          <Button style={buttonStyle} variant="outlined" onClick={() => handleNavigation('/manage-system-admin')}>Managing System Admin</Button>
+          <Button style={buttonStyle} variant="outlined" onClick={() => handleNavigation('/reports-and-statistics')}>Statistics Dashboard</Button>
+          <Button style={buttonStyle} variant="outlined" onClick={() => handleNavigation('/group-request')}>Group Request</Button>
+        </Box>
       </Box>
     </Box>
   );
