e1e8317bcac30c0af644f886a9e35aa6cf0605db
ElyahuAnavi <84002556+elyahu631@users.noreply.github.com>
2023-11-10 07:02:03 +0200

    update 2

--- src/Pages/PHome.jsx
+++ src/Pages/PHome.jsx
@@ -27,17 +27,17 @@ const PHome = () => {
     >
       {user && (
         <Box
-        display="flex"
-        flexDirection={{ xs: 'column', sm: 'row' }}
-        p={2}
-        alignItems="center"
-        gap={2}
-      >        
+          display="flex"
+          flexDirection={{ xs: 'column', sm: 'row' }}
+          p={2}
+          alignItems="center"
+          gap={2}
+        >
           <Typography variant="h5" component="h2" >
             Welcome, {user.first_name} {user.last_name}!
           </Typography>
           <Typography>
-             ({user.email})
+            ({user.email})
           </Typography>
         </Box>
       )}
@@ -55,8 +55,10 @@ const PHome = () => {
           <Button style={buttonStyle} variant="outlined" onClick={() => handleNavigation('/groups')}>Groups</Button>
           <Button style={buttonStyle} variant="outlined" onClick={() => handleNavigation('/tremps')}>Tremps</Button>
         </Box>
-        <Box display="flex" flexDirection="column" gap={2} >
-          <Button style={buttonStyle} variant="outlined" onClick={() => handleNavigation('/manage-system-admin')}>Managing System Admin</Button>
+        <Box display="flex" flexDirection="column" gap={2}>
+          {(user && user.role !== 'helpdesk') && (
+            <Button style={buttonStyle} variant="outlined" onClick={() => handleNavigation('/manage-system-admin')}>Managing System Admin</Button>
+          )}
           <Button style={buttonStyle} variant="outlined" onClick={() => handleNavigation('/reports-and-statistics')}>Statistics Dashboard</Button>
           <Button style={buttonStyle} variant="outlined" onClick={() => handleNavigation('/group-request')}>Group Request</Button>
         </Box>
@@ -66,3 +68,5 @@ const PHome = () => {
 };
 
 export default PHome;
+
+
